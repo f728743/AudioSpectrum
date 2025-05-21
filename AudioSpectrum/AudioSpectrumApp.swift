06f9b09251b7c712bc403bc32377e54b483bf10e
Alexey Vorobyov <vorobyov.alexey@gmail.com>
2025-05-21 08:10:29 +0300

    Initial commit

---
+++ AudioSpectrum/AudioSpectrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  AudioSpectrumApp.swift
+//  AudioSpectrum
+//
+//  Created by Alexey Vorobyov on 21.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct AudioSpectrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AudioSpectrum/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  AudioSpectrum
+//
+//  Created by Alexey Vorobyov on 21.05.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
